01d5e178afd46e8ff6a4186995658ce08fc11e56
soniyaProgramming123 <nitinkumar68746@gmail.com>
2026-06-20 12:27:02 +0530

    Create 6_class_input.c

---
+++ 6_class_input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    //declaring int variable
+    int age;
+    printf("enter your age: ");
+    // taking input for num variable 
+    scanf("%d", &age);
+    //printing the value of num
+    printf("you are %i year old.\n",age);
+}
